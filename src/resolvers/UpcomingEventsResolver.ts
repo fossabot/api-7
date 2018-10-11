06ac0a439c654a631a7585e3b7f65fecabd8c6ed
Christoph Werner <codepunkt@users.noreply.github.com>
2018-10-11 13:57:29 +0200

    ci: allow now deploys via github integration

--- src/resolvers/UpcomingEventsResolver.ts
+++ src/resolvers/UpcomingEventsResolver.ts
@@ -8,8 +8,8 @@ class UpcomingEventsResolver {
   public resolve(): Promise<any> {
     return this.meetupEventsService.fetch('upcoming').then(events =>
       events.map(event => ({
+        date: new Date(event.time),
         goingCount: event.yes_rsvp_count,
-        time: new Date(event.time),
         url: event.link,
         venue: { ...event.venue, street: event.venue.address_1 },
       }))
